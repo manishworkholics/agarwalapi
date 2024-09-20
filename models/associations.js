35195122906c7d16049501211f5aaf54bcc6a98e
viraj kale <virajkaleworkholic@gmail.com>
2024-09-20 18:48:10 +0530

    update 7 pm 200920244

--- models/associations.js
+++ models/associations.js
@@ -6,7 +6,7 @@ const subGroupModel = require('./msgSubGroupModel');
 // Define the associations
 groupModel.hasMany(subGroupModel, { foreignKey: 'msg_group_id' });
 subGroupModel.belongsTo(groupModel, { foreignKey: 'msg_group_id' });
-
+// fsdf
 module.exports = {
   groupModel,
   subGroupModel,
